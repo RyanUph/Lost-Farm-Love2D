9ee137f36777f642b1f687bc5f0e19427c4d7846
Malik Fattaev <malfisq@gmail.com>
2023-03-08 19:56:43 +0500

    Changed files | 1

--- main.lua
+++ main.lua
@@ -5,12 +5,27 @@ function love.load()
     camera = require('libraries/camera')
 
     world = wf.newWorld(0, 0)
+    gameMap = sti('maps/lostFarm1.lua')
     cam = camera()
     player.load()
 
-    -- Player collider
+    -- Player colliders
     player.collider = world:newBSGRectangleCollider(player.x, player.y, 55, 80, 10)
     player.collider:setFixedRotation(true)
+
+    -- Border colliders
+    borders = {}
+    if gameMap.layers["Borders"] then
+        for i, obj in pairs(gameMap.layers["Borders"].objects) do
+            border = world:newRectangleCollider(obj.x, obj.y, obj.width, obj.height)
+            border:setType('static')
+            table.insert(borders, border)
+        end
+    end
+
+    stones = {}
+    sprites = {}
+    sprites.stone = love.graphics.newImage('sprites/stone.png')
 end
 
 function love.update(dt)
@@ -21,7 +36,27 @@ end
 
 function love.draw()
     cam:attach()
+        gameMap:drawLayer(gameMap.layers['Grass'])
+        gameMap:drawLayer(gameMap.layers['Path'])
+        gameMap:drawLayer(gameMap.layers['Fences'])
+        gameMap:drawLayer(gameMap.layers['Water'])
+
+        drawStone(500, 500)
+        drawStone(500, 50)
+
         player.draw()
         world:draw()
     cam:detach()
+end
+
+-- Functions
+function drawStone(x, y)
+    stone = {}
+    stone.x = x
+    stone.y = y
+    stone.collider = world:newBSGRectangleCollider(stone.x, stone.y, 128, 128, 40)
+    stone.collider:setType('static')
+    love.graphics.draw(sprites.stone, stone.x, stone.y, nil, nil, nil, 64, 64)
+
+    table.insert(stones, stone)
 end
